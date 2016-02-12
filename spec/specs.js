0847d59194b7e1d09e8bab1d98997096fab21cd3
NathanRomike <nathanromike@gmail.com>
2016-02-12 08:03:47 -0800

    Add first unit test for Pizza constructor: Red.

--- spec/specs.js
+++ spec/specs.js
@@ -1,1 +1,7 @@
-
+describe('Pizza', function() {
+  it('Creates a new pizza', function() {
+    var newPizza = new Pizza('cheese, pepperoni', 'medium');
+    expect(newPizza.toppings).to.equal('cheese, pepperoni');
+    expect(newPizza.pizzaSize).to.equal('medium');
+  });
+});
